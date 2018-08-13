82c73083af1146cc7d7bfca97c0da32efb5ce321
Sean A Clarke <sean.alexander.clarke@gmail.com>
2018-08-13 12:57:54 -0700

    Update GenerationHandler.py

--- GenerationHandler.py
+++ GenerationHandler.py
@@ -5,10 +5,11 @@ import matplotlib.animation
 
 class GenerationHandler:
 
-    def __init__(self, points, generation_size=20, number_of_generations=20, debug=True):
+    def __init__(self, points, generation_size=20, number_of_generations=20, answer=False, debug=True):
         self.points = points
         self.generation_size = generation_size
         self.number_of_generations = number_of_generations
+        self.answer = answer
         self.debug = debug
         self.current_generation = 0
         self.generations = {}
@@ -16,7 +17,7 @@ class GenerationHandler:
         self.all_solutions = []
         self.best_solution = [0, 0, 0]
         self.heuristics = {}
-        self.repeats = 0
+        self.meta = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
         self.generate_heuristics()
         self.initialize()
         self.evolve()
@@ -148,6 +149,16 @@ class GenerationHandler:
                         exhausted.append(second)
                     continue
             return s
+            
+    def group_swap(self, s, use_heuristics):
+        group_size = randrange(2, len(s) // 2 + 1)
+        start = randrange(0, len(s) - 2 * group_size + 1)
+        first = s[start:start + group_size]
+        second = self.subgroup(s[start+group_size:], group_size, sample(range(0, len(s[start + group_size:]) - group_size + 1), k=1)[0])
+        sstart = s.index(second[0])
+        for i in range(0, group_size):
+            s[start + i], s[sstart + i] = s[sstart + i], s[start + i]
+        return s
 
     def cycle(self, solution, attempts):
         s = solution[:]
@@ -158,29 +169,78 @@ class GenerationHandler:
             if attempts < len(s):
                 self.cycle(s, attempts + 1)
             else:
-                s = self.mutate(0, s)
+                s = self.mutate(randrange(0, 9), s)
         return s
+        
+    def smart_cycle(self, solution):
+        s = solution[:]
+        left, right = s[-1], s[0]
+        f = self.fitness(s[-1:] + s[0:1])
+        for i in range(0, len(s) - 1):
+            if self.fitness(s[i:i+2]) > f:
+                left, right, f = s[i], s[i+1], self.fitness(s[i:i+2])
+        #s = self.mutate(randrange(0, 9), s)
+        while not (s[0] == right and s[-1] == left):
+            for c in range(1, len(s)):
+                s[c] = solution[c-1]
+            s[0] = solution[-1]
+            solution = s[:]
+        return s
+        
+    def flip_ends(self, solution):
+        end = solution[0]
+        for i in range(1, len(solution) - 1):
+            if self.fitness([end, solution[i+1]]) < self.fitness(solution[i:i+2]):
+                solution = list(reversed(solution[0:i+1])) + solution[i+1:]
+                break
+        end = solution[-1]
+        for i in reversed(range(1, len(solution) - 1)):
+            if self.fitness([end, solution[i-1]]) < self.fitness(solution[i-1:i+1]):
+                solution = solution[0:i] + list(reversed(solution[i:]))
+                break
+        return solution
 
     def mutate(self, variant, solution, use_heuristics=True):
-        # variants: 0:best_single_swap, 1:cycle_once, 2:improve_single_swap, 3:cycle_to_outlier, 4:random_single_swap, 5:random_group_swap, 6:split_worst, 7:something_else, 8:full_shuffle
-        if variant != 0:
-            variant = 0
+        # variants: 0:best_swap, 1:cycle_once, 2:improved_swap, 3:smart_cycle, 4:random_swap, 5:group_swap, 6:flip_ends, 7:split_worst, 8:full_shuffle
+        self.meta[variant] += 1
         s = solution[:]
-        if variant == 0: # random_swap
+        
+        if variant == 0: # best_swap
+            """ """
+            s = self.random_swap(s, use_heuristics)
+            
+        if variant == 1: # cycle_once
+            """offsets the order of the coordinates in the solution until a unique solution is given"""
+            s = self.cycle(s, 0)
+            
+        if variant == 2: # improved_swap
+            """searches through the solution for any avoid pairs, then non-prefer pairs, swapping place to have better neighbours"""
+            s = self.random_swap(s, use_heuristics)
+            
+        if variant == 3: # smart_cycle
+            """offsets the order of coordinates until the least fir neighbouring pair are separated"""
+            s = self.smart_cycle(s)
+        
+        if variant == 4: # random_swap
             """randomly chooses two coordinates to swap places in the solution"""
-            self.random_swap(s, use_heuristics)
+            s = self.random_swap(s, use_heuristics)
 
-        if variant > 0: # random_group_swap
+        if variant == 5: # group_swap
             """randomly chooses two groups of coordinates to swap places in the solution"""
-            group_size = randrange(2, len(s) // 2 + 1)
-            start = randrange(0, len(s) - 2 * group_size + 1)
-            first = s[start:start + group_size]
-            second = self.subgroup(s[start+group_size:], group_size, sample(range(0, len(s[start + group_size:]) - group_size + 1), k=1)[0])
-            sstart = s.index(second[0])
-            for i in range(0, group_size):
-                s[start + i], s[sstart + i] = s[sstart + i], s[start + i]
-        if variant == 3:
-            self.cycle(self, s, 0)
+            s = self.group_swap(s, use_heuristics)
+
+        if variant == 6: # flip_ends
+            """ """
+            s = self.flip_ends(s)
+                
+        if variant == 7: # split_worst
+            """"""
+            pass
+            
+        if variant == 8: # full_shuffle
+            """randomly shuffles the solution"""
+            s = sample(self.points, k=len(self.points))
+                
         return s
 
     def crossover(self, variant, f, m):
@@ -216,8 +276,7 @@ class GenerationHandler:
                     mutation_map[max(i, 2) - 2].append(mutation)
                 else:
                     while s in self.all_solutions:
-                        #s = self.mutate(randrange(0, 9), s) #should be this when all mutations implemented
-                        s = sample(self.points, k=len(self.points))
+                        s = self.mutate(randrange(0, 9), s)
                     self.all_solutions.append(s)
                     next_generation.append(s)
         return next_generation
@@ -247,6 +306,10 @@ class GenerationHandler:
         mutation_map[1].append(selection[0][0])
         mutation_map[2].append(selection[0][0])
         mutation_map[3].append(selection[0][0])
+        if self.generation_size > 17:
+            mutation_map[1].append(selection[0][0])
+            mutation_map[2].append(selection[0][0])
+            mutation_map[4].append(selection[0][0])
         crossover_pool.append(selection[0][0])
         crossover_weights.append(20)
         for s in selection[1]:
@@ -264,17 +327,14 @@ class GenerationHandler:
         for s in selection[4]:
             mutation_map[5].append(s)
         mutation_map = self.handle_crossovers(crossover_pool, crossover_weights, mutation_map, self.generation_size - sum(len(l) for l in mutation_map.values()))
-        while sum(len(l) for l in mutation_map.values()) < self.generation_size:
-            new_solution = sample(self.points, k=len(self.points))
-            if new_solution not in self.all_solutions:
-                mutation_map[0].append(new_solution)
-                self.all_solutions.append(new_solution)
         return self.handle_mutations(mutation_map)
 
     def evolve(self):
         while self.current_generation < self.number_of_generations:
             self.evaluate()
             self.log()
+            if round(self.best_solution[1], 10) == round(self.answer, 10):
+                self.number_of_generations = self.current_generation + 1
             selection = self.select()
             self.generation = self.next_generation(selection)
             self.current_generation += 1
@@ -305,7 +365,7 @@ class GenerationHandler:
         for g in range(self.current_generation):
             if self.generations[g]['globalapex'] != self.generations[frames[-1]]['globalapex']:
                 frames.append(g)
-        anim = matplotlib.animation.FuncAnimation(self.fig, self.animate, frames=frames, interval=50, repeat_delay=2500)
+        anim = matplotlib.animation.FuncAnimation(self.fig, self.animate, frames=frames, interval=50, repeat_delay=1500)
         matplotlib.pyplot.show()
 
     def stats(self):
@@ -319,13 +379,14 @@ class GenerationHandler:
             print('  Fittest solution found in generation ' + str(self.best_solution[2]))
             print('  Fittest Solution: ' + str(self.best_solution[0]))
             print('  Fittest Solution Fitness: ' + str(self.best_solution[1]))
+            #for k,v in self.meta.items():
+            #    print(str(k) + ': ' + str(v))
             self.animation()
 
 if __name__ == "__main__":
-    #simple = GenerationHandler(points=[(0,0),(5,0),(0,4),(6,5)], generation_size=20, number_of_generations=20, debug=True)
-    #curve = GenerationHandler(points=[(1,1),(3,1),(6,2),(10,4),(15,8),(21,16)], generation_size=20, number_of_generations=20, debug=False )
-    circle = GenerationHandler(points=[(2,1),(1,3),(3,5),(5,0),(9,5),(8,0),(11,4),(11,1),(12,2)], generation_size=20, number_of_generations=200, debug=False)
-    #spiral = GenerationHandler(points=[(25,26),(26,25),(27,25),(28,26),(28,28),(27,30),(25,31),(23,31),(21,28),(21,24),(23,19),(26,17),(29,18),(31,22),(32,27),(31,33),(28,38),(25,40),(22,39),(19,37)], generation_size=20, number_of_generations=30000, debug=False)
-    #canada = GenerationHandler(points=[(2,2),(4,47),(5,6),(7,15),(8,41),(9,27),(12,17),(18,7),(18,14),(19,27),(22,43),(23,19),(27,23),(31,8),(33,3),(33,11),(37,1),(37,39),(38,11),(39,6),(42,2),(47,9),(48,26),(49,6),(49,12)], generation_size=20, number_of_generations=40, debug=False)
-    #europe = GenerationHandler(points=[(4,87),(12,15),(16,54),(19,14),(19,38),(20,26),(21,51),(25,62),(26,15),(26,48),(29,36),(35,18),(39,28),(39,45),(41,13),(43,6),(46,21),(48,67),(50,55),(51,44),(52,38),(53,30),(55,62),(57,24),(57,29),(62,40),(63,24),(64,0),(67,70),(70,11),(70,47),(73,62),(79,35),(87,53)], generation_size=20, number_of_generations=30000, debug=False)
+    #curve = GenerationHandler(points=[(1,1),(3,1),(6,2),(10,4),(15,8),(21,16)], generation_size=20, number_of_generations=10, answer=26.037537852600806, debug=False)
+    #circle = GenerationHandler(points=[(2,1),(1,3),(3,5),(5,0),(9,5),(8,0),(11,4),(11,1),(12,2)], generation_size=20, number_of_generations=20, answer=20.275399939955413, debug=False)
+    spiral = GenerationHandler(points=[(25,26),(26,25),(27,25),(28,26),(28,28),(27,30),(25,31),(22,30),(20,28),(19,23),(20,19),(22,17),(25,16),(28,16),(31,18),(33,22),(34,27),(33,33),(30,39),(26,41),(22,41),(19,40)], generation_size=20, number_of_generations=300, answer=72.10618461080725, debug=False)
+    #canada = GenerationHandler(points=[(2,2),(4,47),(5,6),(7,15),(8,41),(9,27),(12,17),(18,7),(18,14),(19,27),(22,43),(23,19),(27,23),(31,8),(33,3),(33,11),(37,1),(37,39),(38,11),(39,6),(42,2),(47,9),(48,26),(49,6),(49,12)], generation_size=20, number_of_generations=2000, debug=False) #answer may be 200.04912909882898
+    #europe = GenerationHandler(points=[(4,87),(12,15),(16,54),(19,14),(19,38),(20,26),(21,51),(25,62),(26,15),(26,48),(29,36),(35,18),(39,28),(39,45),(41,13),(43,6),(46,21),(48,67),(50,55),(51,44),(52,38),(53,30),(55,62),(57,24),(57,29),(62,40),(63,24),(64,0),(67,70),(70,11),(70,47),(73,62),(79,35),(87,53)], generation_size=20, number_of_generations=2000, debug=False)
     pass
